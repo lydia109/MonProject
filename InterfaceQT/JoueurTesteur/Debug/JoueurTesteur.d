08f1cfc05254cbba90942186833a32ff28dde1d4
lydia abider <lydiaabider96@gmail.com>
2020-05-17 13:07:39 -0400

    projectQtInterface

---
+++ InterfaceQT/JoueurTesteur/Debug/JoueurTesteur.d
@@ -0,0 +1,19 @@
+JoueurTesteur.o: ../JoueurTesteur.cpp \
+ /home/etudiant/Bureau/TP4/source/Joueur.h \
+ /home/etudiant/Bureau/TP4/source/Personne.h \
+ /home/etudiant/Bureau/TP4/source/Date.h \
+ /home/etudiant/Bureau/TP4/source/ContratException.h \
+ /home/etudiant/Bureau/TP4/source/validationFormat.h \
+ /home/etudiant/Bureau/TP4/source/Date.h
+
+/home/etudiant/Bureau/TP4/source/Joueur.h:
+
+/home/etudiant/Bureau/TP4/source/Personne.h:
+
+/home/etudiant/Bureau/TP4/source/Date.h:
+
+/home/etudiant/Bureau/TP4/source/ContratException.h:
+
+/home/etudiant/Bureau/TP4/source/validationFormat.h:
+
+/home/etudiant/Bureau/TP4/source/Date.h:
